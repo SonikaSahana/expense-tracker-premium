f66892ea1b1e6bfb005260c89424821f5b1c42ff
SonikaSahana <deoghariasonika@gmail.com>
2024-03-02 10:54:54 +0530

    some changes done

--- backend/services/s3services.js
+++ backend/services/s3services.js
@@ -11,7 +11,7 @@ function uploadToS3(data , fileName){
         Bucket: process.env.AWS_BUCKET_NAME,
         Key: fileName,
         Body: data,
-        ACL: 'public-read'
+       // ACL: 'public-read'
     };
 
     return new Promise((resolve , reject)=>{
